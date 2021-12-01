c88804b2859bba0ec3de3d1c999306e5d47ea0e3
Auxilor <william.favierparsons1@gmail.com>
2021-12-01 12:22:06 +0000

    Added ConditionHasFuel validation

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/weapons/conditions/ConditionHasFuel.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/weapons/conditions/ConditionHasFuel.kt
@@ -3,6 +3,7 @@ package com.willfp.ecoweapons.weapons.conditions
 import com.willfp.eco.core.config.interfaces.JSONConfig
 import com.willfp.ecoweapons.weapons.WeaponUtils
 import com.willfp.ecoweapons.weapons.Weapons
+import com.willfp.libreforge.ConfigViolation
 import com.willfp.libreforge.conditions.Condition
 import org.bukkit.entity.Player
 
@@ -10,5 +11,19 @@ class ConditionHasFuel: Condition("has_fuel") {
     override fun isConditionMet(player: Player, config: JSONConfig): Boolean {
         val fuel = Weapons.getByID(config.getString("fuel")) ?: return true
         return WeaponUtils.hasFuelFor(player, fuel)
+    }
+
+    override fun validateConfig(config: JSONConfig): List<ConfigViolation> {
+        val violations = mutableListOf<ConfigViolation>()
+
+        config.getBoolOrNull("fuel")
+            ?: violations.add(
+                ConfigViolation(
+                    "fuel",
+                    "You must specify the fuel required! (The fuel from which weapon, eg reaper_scythe)"
+                )
+            )
+
+        return violations
     }
 }
