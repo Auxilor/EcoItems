524ec78e9f35b103c0b9bcd73004d16f3c868b2a
Auxilor <william.favierparsons1@gmail.com>
2021-12-01 12:02:03 +0000

    Added display to fuel

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/display/WeaponsDisplay.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/display/WeaponsDisplay.kt
@@ -3,8 +3,8 @@ package com.willfp.ecoweapons.display
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.display.DisplayModule
 import com.willfp.eco.core.display.DisplayPriority
-import org.bukkit.inventory.ItemStack
 import com.willfp.ecoweapons.weapons.WeaponUtils
+import org.bukkit.inventory.ItemStack
 
 class WeaponsDisplay(plugin: EcoPlugin) : DisplayModule(plugin, DisplayPriority.LOWEST) {
     override fun display(
@@ -12,17 +12,35 @@ class WeaponsDisplay(plugin: EcoPlugin) : DisplayModule(plugin, DisplayPriority.
         vararg args: Any
     ) {
         val meta = itemStack.itemMeta ?: return
-        val weapon = WeaponUtils.getWeaponFromItem(meta) ?: return
-        val weaponMeta = weapon.itemStack.itemMeta ?: return
-        val lore: MutableList<String> = weaponMeta.lore?.toMutableList() ?: return
+        val isFuel = WeaponUtils.getFuelFromItem(meta) != null
+        val weapon = (
+                if (isFuel) WeaponUtils.getFuelFromItem(meta)
+                else WeaponUtils.getWeaponFromItem(meta)
+                ) ?: return
 
-        if (meta.hasLore()) {
-            lore.addAll(meta.lore ?: return)
+        if (isFuel) {
+            val fuelMeta = weapon.fuelItem.itemMeta ?: return
+            val lore: MutableList<String> = fuelMeta.lore?.toMutableList() ?: return
+
+            if (meta.hasLore()) {
+                lore.addAll(meta.lore ?: return)
+            }
+
+            meta.lore = lore
+            meta.setDisplayName(fuelMeta.displayName)
+            itemStack.itemMeta = meta
+        } else {
+            val weaponMeta = weapon.itemStack.itemMeta ?: return
+            val lore: MutableList<String> = weaponMeta.lore?.toMutableList() ?: return
+
+            if (meta.hasLore()) {
+                lore.addAll(meta.lore ?: return)
+            }
+
+            meta.lore = lore
+            meta.setDisplayName(weaponMeta.displayName)
+            meta.addItemFlags(*weaponMeta.itemFlags.toTypedArray())
+            itemStack.itemMeta = meta
         }
-
-        meta.lore = lore
-        meta.setDisplayName(weaponMeta.displayName)
-        meta.addItemFlags(*weaponMeta.itemFlags.toTypedArray())
-        itemStack.itemMeta = meta
     }
 }
