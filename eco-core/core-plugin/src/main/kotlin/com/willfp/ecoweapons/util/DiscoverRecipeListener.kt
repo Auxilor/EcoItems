7cac122a252367854bb9841e8b7a118eb11e3f42
Auxilor <william.favierparsons1@gmail.com>
2021-12-06 12:27:01 +0000

    Improved DiscoverRecipeListener

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/util/DiscoverRecipeListener.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoweapons/util/DiscoverRecipeListener.kt
@@ -6,17 +6,16 @@ import org.bukkit.Keyed
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.inventory.Recipe
 
 class DiscoverRecipeListener(private val plugin: EcoPlugin) : Listener {
     @EventHandler
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
         if (plugin.configYml.getBool("discover-recipes")) {
-            Bukkit.getServer().recipeIterator().forEachRemaining { recipe: Recipe? ->
-                if (recipe is Keyed) {
-                    val key = recipe.key
-                    if (key.namespace == "ecoweapons") {
+            Bukkit.getServer().recipeIterator().forEachRemaining {
+                if (it is Keyed) {
+                    val key = it.key
+                    if (key.namespace == plugin.name.lowercase()) {
                         if (!key.key.contains("displayed")) {
                             player.discoverRecipe(key)
                         }
