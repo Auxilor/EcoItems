8148eccba40000f457de0ec211de8b325f65f26e
Sedri05 <sedri051@gmail.com>
2022-06-10 23:51:51 +0200

    Oi you broke it again

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/ItemListener.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/ItemListener.kt
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerItemDamageEvent
 import org.bukkit.event.player.PlayerItemHeldEvent
+import kotlin.math.roundToInt
 
 class ItemListener(
     private val plugin: EcoPlugin
@@ -29,14 +30,16 @@ class ItemListener(
     @EventHandler
     fun effectiveDurabilityListener(event: PlayerItemDamageEvent) {
         val ecoItem = ItemUtils.getEcoItem(event.item) ?: return
-        val maxDurability = event.item.type.maxDurability.toInt()
+        val maxDurability = event.item.type.maxDurability.toDouble() //This has to be a double for precise division
+        //otherwise ratio will always be 0,1,2,... or some other int
+        //then the randfloat wont work because ratio will be 0 so it removes durabilty like 1/100000 times
         val ratio = maxDurability / ecoItem.effectiveDurability
         if (ratio < 1) {
             if (NumberUtils.randFloat(0.0, 1.0) > ratio) {
                 event.isCancelled = true
             }
         } else if (ratio > 1) {
-            event.damage *= ratio
+            event.damage *= ratio.toInt()
         }
     }
 }
