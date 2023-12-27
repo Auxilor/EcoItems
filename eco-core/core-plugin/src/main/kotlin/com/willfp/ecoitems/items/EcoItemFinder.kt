29c0844fc355813a078ef280fb9046bb91fbd0ca
Will FP <william.favierparsons1@gmail.com>
2023-12-27 16:50:38 +0100

    Codestyle

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/EcoItemFinder.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/EcoItemFinder.kt
@@ -4,7 +4,7 @@ import com.willfp.libreforge.slot.ItemHolderFinder
 import com.willfp.libreforge.slot.SlotType
 import org.bukkit.inventory.ItemStack
 
-object EcoItemFinder: ItemHolderFinder<EcoItem>() {
+object EcoItemFinder : ItemHolderFinder<EcoItem>() {
     override fun find(item: ItemStack): List<EcoItem> {
         return listOfNotNull(item.ecoItem)
     }
