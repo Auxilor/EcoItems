fa590d687df64e5e5670dc5b485754af00c6140f
Will FP <william.favierparsons1@gmail.com>
2025-10-05 11:49:18 +0100

    Updated FoodComponentHandler to support 1.21.4

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/components/FoodComponentHandler.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/items/components/FoodComponentHandler.kt
@@ -1,7 +1,13 @@
 package com.willfp.ecoitems.items.components
 
 import com.willfp.eco.core.config.interfaces.Config
+import io.papermc.paper.datacomponent.DataComponentTypes
+import io.papermc.paper.datacomponent.item.Consumable
+import io.papermc.paper.datacomponent.item.consumable.ConsumeEffect
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
 import org.bukkit.inventory.ItemStack
+import org.bukkit.potion.PotionEffect
 
 @Suppress("UnstableApiUsage")
 object FoodComponentHandler : ComponentHandler("food") {
@@ -16,7 +22,42 @@ object FoodComponentHandler : ComponentHandler("food") {
             food.setCanAlwaysEat(config.getBool("can-always-eat"))
         }
 
+        val eatSeconds = config.getDouble("eat-seconds").toFloat()
+
+        val effects = mutableMapOf<PotionEffect, Float>()
+        for (effectConfig in config.getSubsections("effects")) {
+            val effect = Registry.POTION_EFFECT_TYPE.get(
+                NamespacedKey.minecraft(effectConfig.getString("effect").lowercase())
+            ) ?: continue
+
+            val amplifier = effectConfig.getInt("level") - 1
+            val duration = effectConfig.getInt("duration")
+
+            val ambient = effectConfig.getBoolOrNull("ambient") ?: true
+            val particles = effectConfig.getBoolOrNull("particles") ?: true
+            val icon = effectConfig.getBoolOrNull("icon") ?: true
+
+            val probability = (effectConfig.getDoubleOrNull("probability") ?: 100.0).toFloat() / 100f
+
+
+            effects[PotionEffect(
+                effect,
+                duration,
+                amplifier,
+                ambient,
+                particles,
+                icon
+            )] = probability
+
+        }
+        val consumeEffects = effects.map { ConsumeEffect.applyStatusEffects(listOf(it.key), it.value) }
+
+        val consumable = Consumable.consumable()
+            .consumeSeconds(eatSeconds)
+            .addEffects(consumeEffects)
+
         meta.setFood(food)
         item.itemMeta = meta
+        item.setData(DataComponentTypes.CONSUMABLE, consumable)
     }
 }
