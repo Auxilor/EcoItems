1b04d11c1aec3fe8756e3f9782a4754fee9299a0
NicoNeko <nicuch100@gmail.com>
2026-03-09 08:37:32 +0200

    Relocate kotlin Add time to reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/commands/CommandReload.kt
@@ -1,12 +1,15 @@
 package com.willfp.ecoitems.commands
 
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
 import com.willfp.ecoitems.plugin
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(plugin, "reload", "ecoitems.command.reload", false) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        plugin.reload()
-        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+        sender.sendMessage(
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toString())
+        )
     }
 }
