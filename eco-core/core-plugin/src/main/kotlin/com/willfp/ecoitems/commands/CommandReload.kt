540cda608d7c80399532984f2010dfa78803afb7
NicoNeko <nicuch100@gmail.com>
2026-04-24 22:49:31 +0300

    Add folia support (#99)
    Co-authored-by: Callum Johnson <74352969+CallumJohnson@users.noreply.github.com>

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoitems/commands/CommandReload.kt
@@ -1,20 +1,25 @@
 package com.willfp.ecoitems.commands
 
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
-import com.willfp.eco.core.recipe.Recipes
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
 import com.willfp.ecoitems.items.EcoItems
 import com.willfp.ecoitems.plugin
 import org.bukkit.command.CommandSender
-import org.bukkit.inventory.Recipe
 
 object CommandReload : Subcommand(plugin, "reload", "ecoitems.command.reload", false) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", plugin.reloadWithTime().toNiceString())
-                .replace("%count%", EcoItems.values().size.toString())
-        )
+        val runnable: Runnable = {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+                    .replace("%count%", EcoItems.values().size.toString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable) // run on global thread
+        else
+            runnable.run()
     }
 }
